b28370129277a36aa4f7c40511ad5aa63ec67295
Alejandro Alberola Gopar <93872954+alberola@users.noreply.github.com>
2023-04-26 17:18:51 +0100

    Fixed

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -2,10 +2,10 @@
 $(".moreProjectsContainer").hide();
 $("#showMoreProjects").click(function(){
     if ($("#showMoreProjects").text() == "Show More ↘") {
-        $(".moreProjectsContainer").show(1000);
+        $(".moreProjectsContainer").slideDown(1000);
         $("#showMoreProjects").text("Show Less ↖");
     } else {
-        $(".moreProjectsContainer").hide(1000);
+        $(".moreProjectsContainer").slideUp(1000);
         $("#showMoreProjects").text("Show More ↘");
     }
 });
